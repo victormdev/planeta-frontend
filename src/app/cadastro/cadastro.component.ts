172df2afc76bfcfac18a181b30de9a80cf61a2c1
Victor Macedo <victoralexandrino99@gmail.com>
2024-07-07 23:23:10 -0300

    correção de cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
@@ -5,10 +6,18 @@ import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, RouterLinkActive],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, CommonModule],
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css', '../../assets/css/nucleo-icons.css', '../../assets/css/nucleo-icons.css', '../../assets/css/nucleo-svg.css']
 })
 export class CadastroComponent {
+  selectedTab = 1;
 
+  selecttab(tabNumber: number) {
+    this.selectedTab = tabNumber;
+  }
+
+  isSelected(tabNumber: number) {
+    return this.selectedTab === tabNumber;
+  }
 }
