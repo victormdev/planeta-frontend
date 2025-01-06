d9b4c4507715786ff5f5fd2bfd30d87171da6ca4
victormdev <vmadevops@gmail.com>
2025-01-06 02:55:47 -0300

    update new design

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
@@ -10,7 +11,31 @@ import { RouterLink, RouterOutlet, RouterLinkActive, Router } from '@angular/rou
   styleUrls: ['./dashboard.component.css', '../../assets/css/nucleo-icons.css', '../../assets/css/nucleo-icons.css', '../../assets/css/nucleo-svg.css']
 })
 export class DashboardComponent {
-  constructor(private router: Router){}
+  userId: string = '';
+  pjnomeCompleto: string = ''; // Para armazenar o nome completo
+
+  constructor(private router: Router, private authService: AuthService){}
+
+  ngOnInit() {
+    // Obtém o userId do localStorage
+    this.userId = localStorage.getItem('userId') || '';
+
+    // Verifica se o userId está definido
+    if (this.userId) {
+      this.authService.getUserPJDetails(this.userId).subscribe(
+        userDetailsPj => {
+          // Obtém o nome completo do usuário
+          this.pjnomeCompleto = userDetailsPj.nome_completo;
+        },
+        error => {
+          console.error('Erro ao obter os detalhes do usuário:', error);
+        }
+      );
+    } else {
+      console.error('Nenhum userId encontrado no localStorage.');
+    }
+  }
+  
   logout() {
     localStorage.removeItem('angularLogin'); // Remove o token do localStorage
     this.router.navigateByUrl('/login'); // Redireciona para a tela de login
